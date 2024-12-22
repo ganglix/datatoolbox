99f23a7b253ac300e0f154198a2b318faf3bcd3a
Gang <ganglix@gmail.com>
2024-12-21 23:36:33 -0600

    packaged

---
+++ datatoolbox/__init__.py
@@ -0,0 +1,6 @@
+# Import main functionalities to make them accessible at the package level
+from .fit_general_poly_model import polynomial_model, fit_and_calculate_intervals
+from .fit_spline_model import fit_spline_with_confidence
+
+# Versioning
+__version__ = "0.1.0"
